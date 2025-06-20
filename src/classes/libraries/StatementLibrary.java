4f5e537b6a0abc80552bc92aedca9dbe1dc5df3b
BroerSakki <besterizak@gmail.com>
2025-06-20 19:27:29 +0200

    Statement version 1.0.2

--- src/classes/libraries/StatementLibrary.java
+++ src/classes/libraries/StatementLibrary.java
@@ -1,5 +1,5 @@
 // Package all statements in custom library
-package  classes.libraries;
+package classes.libraries;
 
 // Import Java CLasses
 
@@ -7,6 +7,10 @@ public interface StatementLibrary {
     // Keywords
         String KEY_START = "start";
         String KEY_STOP = "stop";
+        String KEY_PUBLIC = "public";
+        String KEY_PRIVATE = "private";
+        String KEY_PROTECTED = "protected";
+        String KEY_STATIC = "static";
         String KEY_RETURN = "return";
         String KEY_TRUE = "true";
         String KEY_FALSE = "false";
@@ -21,9 +25,11 @@ public interface StatementLibrary {
         String KEY_DECLARATIONS = "Declarations";
         String KEY_TYPE_NUM = "num";
         String KEY_TYPE_STRING = "string";
+        String KEY_TYPE_BOOLEAN = "boolean";
         String KEY_INPUT = "input";
         String KEY_OUTPUT = "output";
 
-    // Keyword list
-    String[] KEYS_STATEMENT_TYPE = {KEY_START, KEY_STOP, KEY_RETURN, KEY_WHILE, KEY_ENDWHILE, KEY_FOR, KEY_ENDFOR, KEY_IF, KEY_ELSE, KEY_ELSE_IF, KEY_ENDIF, KEY_DECLARATIONS, KEY_TYPE_STRING, KEY_TYPE_NUM, KEY_INPUT, KEY_OUTPUT};
+    // Keyword lists
+    String[] KEYS_STATEMENT_TYPE = {KEY_START, KEY_STOP, KEY_PUBLIC, KEY_PRIVATE, KEY_PROTECTED, KEY_STATIC, KEY_RETURN, KEY_WHILE, KEY_ENDWHILE, KEY_FOR, KEY_ENDFOR, KEY_IF, KEY_ELSE, KEY_ELSE_IF, KEY_ENDIF, KEY_DECLARATIONS, KEY_TYPE_STRING, KEY_TYPE_NUM, KEY_TYPE_BOOLEAN, KEY_INPUT, KEY_OUTPUT};
+    String[] KEYS_STATEMENT_DECLARATION_START = {KEY_PUBLIC, KEY_PRIVATE, KEY_PROTECTED};
 }

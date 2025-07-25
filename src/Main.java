3f6bed6ae50e221ac20fe002c822d56a30b484f9
BroerSakki <besterizak@gmail.com>
2025-07-25 14:59:38 +0200

    Auto-sync Fri Jul 25 14:59:37 SAST 2025

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import classes.datatypes.Num;
+import classes.datatypes.Statement;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		String[] values = {"3.14", "2.71", "1.41"};
+		Num numArray = new Num(values);
+
+		Num numSingle = new Num("42");
+
+		System.out.println("Single Num value: " + numSingle);
+		System.out.println("Array Num values: " + numArray);
+
+		Statement statement = new Statement("public static void main(String[] args)");
+		System.out.println("Statement text: " + statement.toString());
 	}
 }
